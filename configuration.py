0b845aa4fc593d87675d44f919c97dcc3d5b32dd
Valeriia Talerova <leratalerova@yandex.ru>
2026-02-20 18:03:30 +0300

    Добавила скриншот

--- configuration.py
+++ configuration.py
@@ -1,3 +1,3 @@
-URL_SERVICE = "https://e38dfa2d-918c-4585-af4f-b7333312b382.serverhub.praktikum-services.ru"
+URL_SERVICE = "https://5bcbe436-b3fa-475d-b40b-c6435554ca56.serverhub.praktikum-services.ru"
 CREATE_ORDER_PATH = "/api/v1/orders/"
 ORDER_TRACK_PATH = "/api/v1/orders/track"
--- sender_stand_request.py
+++ sender_stand_request.py
@@ -1,13 +1,13 @@
 import configuration
 import requests
 import data 
+
 def create_order(order_body):
       return requests.post(
         configuration.URL_SERVICE + configuration.CREATE_ORDER_PATH,
         json=order_body,
         headers=data.headers
     )
-
 
 def get_order_by_track_number(track_number):
     params = {'t': track_number}
